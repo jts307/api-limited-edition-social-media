ec4a8a7605a3a2afbe86a88b21203113f7bb3ffe
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2021-05-22 13:10:27 -0700

    displayname

--- src/models/post_model.js
+++ src/models/post_model.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 // Adapted from express-mongo-server side SA
 
-// edited to create a "post" that has a caption, content, 
+// edited to create a "post" that has a caption, content,
 // which will probably be in the form of a url, an author, which is a User object, viewLimit and current Views
 // added in .env to test auth secrets
 const PostSchema = new Schema({
