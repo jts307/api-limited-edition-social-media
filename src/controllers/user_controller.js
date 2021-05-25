6bc855ddd511b9ec9f75e0df8b3fe06e9ff93291
Jacob E Werzinsky <f003frq@dartmouth.edu>
2021-05-25 18:04:14 -0400

    finished, just checking if it works with 99.9% certainty

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -9,13 +9,13 @@ export const signin = (user) => {
   return tokenForUser(user);
 };
 
-// note the lovely destructuring here indicating that we are passing in an object with these 3 keys
+// note the lovely destructuring here indicating that we are passing in an object with these 4 keys
 export const signup = async ({
   email, password, displayname, username,
 }) => {
   // No email and pw
   if (!email || !password || !displayname || !username) {
-    throw new Error('You must provide email and password');
+    throw new Error('Please fill out all fields.');
   }
 
   // Check if a user with the given email address exists
