3601db6cccadc76e5adedd038011512fad5446e7
Jacob Werzinsky <f003frq@dartmouth.edu>
2021-06-08 14:54:42 -0400

    Update router.js

--- src/router.js
+++ src/router.js
@@ -137,8 +137,8 @@ router.put('/profile', async (req, res) => {
   try {
     const { sub } = jwt.decode(req.headers.authorization, process.env.AUTH_SECRET);
     const user = await UserController.getUser(sub);
-    const pfp = await UserController.updateProfilePic(user.id, req.body.profileUrl);
-    res.json(pfp);
+    await UserController.updateProfilePic(user.id, req.body.profileUrl);
+    res.json(user);
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: error.toString() });
